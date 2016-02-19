15fdad13c84dace1f51f9737568ff72cc3a1fa61
John Rodrigues <john.rodrigues@motio.com>
2016-02-19 09:57:12 -0600

    Updates after running lein kibit.

--- src/com/texanops/algos/sorting.clj
+++ src/com/texanops/algos/sorting.clj
@@ -20,20 +20,20 @@
 (defn quicksort
   "Quicksort algorithm"
   ([inp]
-     (let [inp-length (count inp)]
-       (if (empty? inp)
-         inp
-         (let [[pivot-elt & other-elts] inp
-               less-than-equal-pivot (filter #(>= pivot-elt %1) other-elts)
-               greater-than-pivot (filter #(< pivot-elt %1) other-elts)]
-           (lazy-cat (quicksort less-than-equal-pivot)
-                     [pivot-elt]
-                     (quicksort greater-than-pivot)))))))
+   (let [inp-length (count inp)]
+     (if (empty? inp)
+       inp
+       (let [[pivot-elt & other-elts] inp
+             less-than-equal-pivot (filter #(>= pivot-elt %1) other-elts)
+             greater-than-pivot (filter #(< pivot-elt %1) other-elts)]
+         (lazy-cat (quicksort less-than-equal-pivot)
+           [pivot-elt]
+           (quicksort greater-than-pivot)))))))
 
 (defn benchmark-sorts
   "Benchmark the various sorting functions"
   []
-  (let [inp (take 1000 (repeatedly #(int (rand 1000))))
+  (let [inp (repeatedly 1000 #(int (rand 1000)))
         srt-1 (time (do (println "Quicksort")
                         (quicksort inp)))
         srt-2 (time (do (println "Bubblesort")
