7d688eb6776a6017d7fb4996eafad52999b702f1
John Rodrigues <john.rodrigues@motio.com>
2016-02-19 13:26:27 -0600

    Progressive development.

--- src/com/texanops/algos/content_analyzer.clj
+++ src/com/texanops/algos/content_analyzer.clj
@@ -1,6 +1,8 @@
 (ns com.texanops.algos.content-analyzer
   (:require [com.texanops.algos.utils :refer [get-content-file]]))
 
+;; Looked for the FAILED key in the result. Indicates what failed the regex.
+;; Also, this is equating words like to-morrow and to-night.
 (defn remove-punctuation [word]
   (try
     (let [word-without-punctuation
@@ -14,6 +16,7 @@
         [word-count word-variations] (get recorded-words word-without-punctuation [0 #{}])]
     (assoc recorded-words word-without-punctuation [(inc word-count) (conj word-variations word)])))
 
+;; Removes punctuation and double hypens from the input.
 (defn sanitize-input [input]
   (clojure.string/replace
     (clojure.string/replace input #"[!?:.;,]" " ")
@@ -24,9 +27,12 @@
    (analyze-content (get-content-file)))
   ([file-name]
    (let [all-words (clojure.string/split (sanitize-input (slurp file-name)) #"\s+")
-         longer-words (filter #(> (count %) 4) all-words)
-         word-counts (reduce record-word {} longer-words)]
-     (into (sorted-map-by (fn [key1 key2]
-                            (compare (first (word-counts key2))
-                              (first (word-counts key1)))))
-       word-counts))))
+         words-with-more-than-4chars (filter #(> (count %) 4) all-words)
+         word-stats (reduce record-word {} words-with-more-than-4chars)
+         words-sorted-by-frequency (sort #(let [[k1 [wc1 wvars1]] %1
+                                                [k2 [wc2 wvars2]] %2]
+                                            (> wc1 wc2)) word-stats)
+         result (java.util.LinkedHashMap.)]
+     (doseq [[key value] words-sorted-by-frequency]
+       (.put result key value))
+     result)))
